dbf4f8a56ec3c94f522b3c3b6cea5503cfea23ce
fartun <fartunwario34@gmail.com>
2023-06-24 01:21:52 +0300

    completed assignment

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,12 +7,13 @@ class PostsController < ApplicationController
   end
 
   def update
-    post = Post.find(params[:id])
-
-    post.update(post_params)
-
-    render json: post
-  end
+     post = Post.find(params[:id])
+      if post.update(post_params)
+        render json: post
+      else
+        render json: { errors: post.errors.full_messages }, status: :unprocessable_entity
+      end
+    end
 
   private
 
